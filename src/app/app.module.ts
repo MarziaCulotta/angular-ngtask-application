cdf62a67d1f935fa2e2d355377f0fe34f484890a
Marzia Culotta <[marzy90@gmail.com]>
2021-11-28 16:17:56 +0100

    feat: add ProjectModule + lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProjectDashboardComponent } from './modules/project/pages/project-dashboard/project-dashboard.component';
-import { ProjectDetailComponent } from './modules/project/pages/project-detail/project-detail.component';
-import { ProjectListComponent } from './modules/project/components/project-list/project-list.component';
-import { ProjectFormComponent } from './modules/project/components/project-form/project-form.component';
 import { HomeComponent } from './modules/dashboard/pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './layout/navbar/navbar.component';
-import { SharedModule } from './shared/shared.module';
+import { ProjectModule } from './modules/project/project.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectDashboardComponent,
-    ProjectDetailComponent,
-    ProjectListComponent,
-    ProjectFormComponent,
     HomeComponent,
     NavbarComponent,
 
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    SharedModule,
     AppRoutingModule
   ],
   providers: [],
